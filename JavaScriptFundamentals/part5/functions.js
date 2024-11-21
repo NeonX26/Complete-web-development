7fdfa5bcfb8262203083c41d4cf59f54f6fc67fa
vishalj2023@outlook.com <vishalj2023@outlook.com>
2024-11-21 23:57:48 +0530

    .

--- JavaScriptFundamentals/part5/functions.js
+++ JavaScriptFundamentals/part5/functions.js
@@ -22,4 +22,30 @@ const greetArrow = (name) => {
 
 
 const greetArrow2 = name => `Hello ${name}`;
-console.log(greetArrow2("Vishal"))
+// console.log(greetArrow2("Vishal"))
+
+
+// higher order functions
+// functions that take other functions as arguments
+// functions that return functions
+function makeTea(typeOfTea){
+    return `Making ${typeOfTea}`
+}
+
+function processTeaOrder(teaFunction) {
+    return teaFunction("Green Tea")
+}
+
+let Order = processTeaOrder(makeTea)    // Making Green Tea
+console.log(Order)
+
+
+// function returning function
+function createTeaMaker(name) {
+    return function (teaType) {
+        return `Making ${teaType}`
+    }
+}
+let teaMaker = createTeaMaker("vishal")
+let result = teaMaker("Green Tea")
+console.log(result)
